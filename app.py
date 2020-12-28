384f97b90e35495ae0ad1e8b772f2a9fc9ac1baa
SanthoshKumar92-eng <65111330+SanthoshKumar92-eng@users.noreply.github.com>
2020-12-28 11:23:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
 import pickle
+import os
 
 
 app = Flask(__name__)
@@ -36,4 +37,6 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug= True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    #app.run(host='0.0.0.0',debug= True)
